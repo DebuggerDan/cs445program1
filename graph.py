4ad277b2ac23c9f5f45ad3ad763c17a99ad152d1
dan <68046886+DebuggerDan@users.noreply.github.com>
2021-07-12 21:31:04 -0700

    test

--- graph.py
+++ graph.py
@@ -2,14 +2,14 @@
 # Graph Implementation
 
 
-import numpy as nump
+import numpy
 import matplotlib.pyplot as mplot
 import matplotlib
 matplotlib.use('Agg')
 
 def graph(n, momentum):
-    x, y = nump.loadtxt("trainingaccuracy.csv", delimiter = ',', unpack=True)
-    x2, y2 = nump.loadtxt("testingaccuracy.csv", delimiter = ',', unpack=True)
+    x, y = numpy.loadtxt("trainingaccuracy.csv", delimiter = ',', unpack=True)
+    x2, y2 = numpy.loadtxt("testingaccuracy.csv", delimiter = ',', unpack=True)
 
     mplot.plot(x, y, label = "Accuracy of the Training Set")
     mplot.plot(x2, y2, label = "Accuracy of the Testing Set")
--- two-layer-neural-net.py
+++ two-layer-neural-net.py
@@ -2,7 +2,7 @@
 # Two-Layer Neural Net Perceptron for MNIST Input for 784 Inputs & 10 Outputs, 1 Hidden Layer
 
 
-import numpy as nump
+import numpy
 import csv
 from sklearn.metrics import confusion_matrix
 import graph
@@ -15,37 +15,37 @@ matplotlib.use('Agg')
 
 trainfile = open("mnist_train.csv", "r")
 trainset = csv.reader(trainfile)
-traindata = nump.array(list(trainset))
+traindata = numpy.array(list(trainset))
 
 testfile = open("mnist_test.csv", "r")
 testset = csv.reader(testfile)
-testdata = nump.array(list(testset))
+testdata = numpy.array(list(testset))
 
 
 # Setup: Variables & Parameters for Experiments #1 and #2
 
 inputs = 784
-# The numpber of inputs!
+# The number of inputs!
 momentum = 0.9
 # The momentum!
 learnrate = 0.1
 # The learning rate!
 epochs = 50
-# The numpber of epoch(s)! [E.g. 'cycles']
+# The number of epoch(s)! [E.g. 'cycles']
 n = 20
 # n, hidden units
 bi = 1
 # The bias!
-confusion = nump.zeros((10, 10), dtype = int)
+confusion = numpy.zeros((10, 10), dtype = int)
 # The confusion matrix constant!
 
 
 # Setup: Sigmoid Function [Classification: Activation Function]
 
 def activationsigmoid(n):
-    return 1 / (1 + nump.exp(-n))
+    return 1 / (1 + numpy.exp(-n))
 
-activations = nump.zeros((1, n + 1))
+activations = numpy.zeros((1, n + 1))
 activations[0,0] = 1
 
 
@@ -54,22 +54,22 @@ activations[0,0] = 1
 nw = -0.05
 pw = 0.05
 
-weightinput = nump.random.uniform(nw, pw,(inputs, n))
-pweightinput = nump.zeros((inputs, n))
+weightinput = numpy.random.uniform(nw, pw,(inputs, n))
+pweightinput = numpy.zeros((inputs, n))
 
-weightoutput = nump.random.uniform(nw, pw, (n + 1, 10))
-pweeighoutput = nump.zeros((n + 1, 10))
+weightoutput = numpy.random.uniform(nw, pw, (n + 1, 10))
+pweeighoutput = numpy.zeros((n + 1, 10))
 
 
 # Pre-Start: Arrays for runtime data array(s) for both training and test runs (epochs/experiments)
 
 # prep = 255
 
-trainrundata = nump.asfarray(traindata[:, 1:]) / 255
-trainlabeldata = nump.asfarray(traindata[:, 1:])
+trainrundata = numpy.asfarray(traindata[:, 1:]) / 255
+trainlabeldata = numpy.asfarray(traindata[:, 1:])
 
-testrundata = nump.asfarray(testdata[:, 1:]) / 255
-testlabeldata = nump.asfarray(testdata[:, 1:])
+testrundata = numpy.asfarray(testdata[:, 1:]) / 255
+testlabeldata = numpy.asfarray(testdata[:, 1:])
 
 
 # Pre-Start: Task Settings (By k-th unit)
@@ -77,13 +77,14 @@ testlabeldata = nump.asfarray(testdata[:, 1:])
 # k = 0.9
 # other = 0.1
 
-target = nump.arange(10)
+target = numpy.arange(10)
 
-traintk = (target == trainlabeldata).astype(nump.float)
+traintk = (target == trainlabeldata).astype(numpy.float)
+testtk = (target == testlabeldata).astype(numpy.float)
 traintk[traintk == 0] = 0.1
 traintk[traintk == 1] = 0.9
 
-testtk = (target == testlabeldata).astype(nump.float)
+
 testtk[testtk == 0] = 0.1
 testtk[testtk == 1] = 0.9
 
@@ -111,23 +112,23 @@ def twolayerperceptron(epoch, label, dataset, task, config):
 
         # Start (Loop): Activation of the Output and Hidden Layers
 
-        zhidden = nump.dot(x, weightinput)
+        zhidden = numpy.dot(x, weightinput)
         sigmoidhidden = activationsigmoid(zhidden)
         
         activations[0, 1:] = sigmoidhidden
 
-        zoutput = nump.dot(activations, weightoutput)
+        zoutput = numpy.dot(activations, weightoutput)
         sigmoidoutput = activationsigmoid(zoutput)
 
         # Start (Loop): Finding the maximum argument and then appending it to predict-array
 
-        prediction = nump.argmax(sigmoidoutput)
+        prediction = numpy.argmax(sigmoidoutput)
         predicta.append(prediction)
 
         # Intensive (Training Perceptrons)
         if config == 1 and epoch > 0: # If we are currently on k+1'th epoch [cycle] and config has been enabled
             outputerror = sigmoidoutput * (1 - sigmoidoutput) * (task[idx] - sigmoidoutput)
-            hiddenerror = sigmoidhidden * (1 - sigmoidhidden) * nump.dot(outputerror, weightoutput[1:,:].T)
+            hiddenerror = sigmoidhidden * (1 - sigmoidhidden) * numpy.dot(outputerror, weightoutput[1:,:].T)
 
             deltainput = (learnrate * hiddenerror * x.T) + (momentum * pweightinput)
             pweighinput = deltainput
@@ -139,18 +140,18 @@ def twolayerperceptron(epoch, label, dataset, task, config):
 
         
     # Start: Accuracy Measurement
-    accuracyv = (nump.array(predicta) == nump.array(actuala)).sum() / float(len(actuals))
+    accuracyv = (numpy.array(predicta) == numpy.array(actuala)).sum() / float(len(actuals))
     acc = accuracyv * 100
 
     if config == 1 and epoch == (epochs - 1): # If we are currently testing & If testing is complete
         genconfusion = confusion_matrix(actuala, predicta)
-        confusion = nump.add(genconfusion, confusion)
+        confusion = numpy.add(genconfusion, confusion)
         print("Epoch #", epochs, " - Confusion Matrix")
         print(confusion)
 
     if config == 1 and epoch != (epochs - 1): # If we are currently testing & If testing is not complete
         genconfusion = confusion_matrix(actuala, predicta)
-        confusion = nump.add(genconfusion, confusion)
+        confusion = numpy.add(genconfusion, confusion)
 
     return acc
 
